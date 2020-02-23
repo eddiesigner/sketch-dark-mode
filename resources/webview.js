c1c495f27664dfac3bc335a705709027fac43872
Eduardo Gomez <this.eduardo@gmail.com>
2020-02-23 23:56:40 +0100

    Add webview dependencies

---
+++ resources/webview.js
@@ -0,0 +1,4 @@
+// disable the context menu (eg. the right click menu) to have a more native feel
+document.addEventListener('contextmenu', (e) => {
+  e.preventDefault()
+})
--- src/create-palette.js
+++ src/create-palette.js
@@ -1,6 +1,40 @@
 import BrowserWindow from 'sketch-module-web-view'
+import { getWebview } from 'sketch-module-web-view/remote'
 import UI from 'sketch/ui'
 
+const webviewIdentifier = 'sketch-dark-mode.webview'
+
+// When the plugin is shutdown by Sketch (for example when the user disable the plugin)
+// we need to close the webview if it's open
+export function onShutdown() {
+  const existingWebview = getWebview(webviewIdentifier)
+  if (existingWebview) {
+    existingWebview.close()
+  }
+}
+
 export default () => {
-  UI.message('UI loaded!')
+  const windowOptions = {
+    identifier: webviewIdentifier,
+    width: 375,
+    height: 667,
+    titleBarStyle: 'hidden',
+    show: false
+  }
+
+  const browserWindow = new BrowserWindow(windowOptions)
+
+  // only show the window when the page has loaded to avoid a white flash
+  browserWindow.once('ready-to-show', () => {
+    browserWindow.show()
+  })
+
+  const webContents = browserWindow.webContents
+
+  // print a message when the page loads
+  webContents.on('did-finish-load', () => {
+    UI.message('UI loaded!')
+  })
+
+  browserWindow.loadURL(require('../resources/webview.html'))
 }
