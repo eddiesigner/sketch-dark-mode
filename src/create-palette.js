bc2a7e4ae0a0abddbae6bdc78c7d0529a4bbc0fb
Eduardo Gomez <eduardo@newsadoo.com>
2020-02-25 17:23:55 +0100

    Delete unecessary import

--- src/create-palette.js
+++ src/create-palette.js
@@ -1,6 +1,5 @@
 import BrowserWindow from 'sketch-module-web-view'
 import { getWebview } from 'sketch-module-web-view/remote'
-import UI from 'sketch/ui'
 
 const webviewIdentifier = 'sketch-dark-mode.webview'
 
