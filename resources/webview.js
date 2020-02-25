237376e1cd45883040dc896ae90b16a01ea1f914
Eduardo Gomez <this.eduardo@gmail.com>
2020-02-25 22:13:25 +0100

    Build Webview UI based on document colors

--- resources/webview.js
+++ resources/webview.js
@@ -1,3 +1,5 @@
+import $ from './jquery'
+
 const interceptClickEvent = (event) => {
   const target = event.target.closest('a')
 
@@ -7,10 +9,45 @@ const interceptClickEvent = (event) => {
   }
 }
 
+window.createPaletteUI = (documentColors) => {
+  const $colorsList = $('.js-colors-list')
+
+  if (documentColors.length > 0) {
+    const $colorThemePrototype = $('.js-color-theme-prototype')
+
+    documentColors.forEach((documentColor) => {
+      const $colorThemeInstance = $colorThemePrototype
+        .first()
+        .clone()
+        .appendTo('.js-colors-list')
+        .removeClass('js-color-theme-prototype')
+        .addClass('js-color-theme')
+
+      $colorThemeInstance
+        .find('.js-color-theme-name')
+        .text(documentColor.name)
+
+      const colorValue = documentColor.color.slice(0, -2)
+
+      $colorThemeInstance
+        .find('.js-color-theme-input-light')
+        .val(colorValue)
+
+      $colorThemeInstance
+        .find('.js-color-theme-preview-light')
+        .css('background-color', colorValue)
+    })
+
+    $colorThemePrototype.remove()
+  } else {
+    $colorsList.hide()
+  }
+}
+
 // listen for link click events at the document level
 document.addEventListener('click', interceptClickEvent)
 
 // disable the context menu (eg. the right click menu) to have a more native feel
 document.addEventListener('contextmenu', (e) => {
-  e.preventDefault()
+  // e.preventDefault()
 })
--- src/create-palette.js
+++ src/create-palette.js
@@ -1,7 +1,10 @@
+import sketch from 'sketch/dom'
 import BrowserWindow from 'sketch-module-web-view'
 import { getWebview } from 'sketch-module-web-view/remote'
 
 const webviewIdentifier = 'sketch-dark-mode.webview'
+const doc = sketch.getSelectedDocument()
+const documentColors = doc.colors.length > 0 ? doc.colors : []
 
 // When the plugin is shutdown by Sketch (for example when the user disable the plugin)
 // we need to close the webview if it's open
@@ -17,12 +20,18 @@ export default () => {
     identifier: webviewIdentifier,
     width: 460,
     minWidth: 460,
-    maxWidth: 740,
+    maxWidth: 460,
     height: 740,
     minHeight: 460,
     maxHeight: 960,
     titleBarStyle: 'hidden',
-    show: false
+    show: false,
+    fullscreenable: false,
+    hidesOnDeactivate: false,
+    remembersWindowFrame: true,
+    webPreferences: {
+      devTools: true
+    }
   }
 
   const browserWindow = new BrowserWindow(windowOptions)
@@ -37,6 +46,16 @@ export default () => {
   webContents.on('externalLinkClicked', (url) => {
     NSWorkspace.sharedWorkspace().openURL(NSURL.URLWithString(url))
   })
+
+  webContents.executeJavaScript(
+    `createPaletteUI(${JSON.stringify(documentColors)})`
+  )
+    .then((res) => {
+      console.log(res)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 
   browserWindow.loadURL(require('../resources/webview.html'))
 }
