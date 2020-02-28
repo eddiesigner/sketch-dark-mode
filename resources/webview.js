2ea4772b5323896e4f4ca5c0f9172b54bf123a60
Eduardo Gomez <this.eduardo@gmail.com>
2020-02-29 00:24:19 +0100

    Remove comment

--- resources/webview.js
+++ resources/webview.js
@@ -153,5 +153,5 @@ document.addEventListener('click', interceptClickEvent)
 
 // disable the context menu (eg. the right click menu) to have a more native feel
 document.addEventListener('contextmenu', (e) => {
-  // e.preventDefault()
+  e.preventDefault()
 })
